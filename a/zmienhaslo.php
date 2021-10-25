cfbf9756485358780b6bceade2d0380eb6257900
kubusq233 <92712561+kubusq233@users.noreply.github.com>
2021-10-25 13:10:37 +0200

    Add files via upload

--- a/zmienhaslo.php
+++ a/zmienhaslo.php
@@ -23,15 +23,25 @@
 	
 	$row = mysqli_fetch_assoc($q);
 	
-	if($row['HASLO'] == $h1) //czy stare haslo zgadza sie tym z bazy danych
-		{
-			$z2 = "UPDATE uzytkownik SET haslo='$h2' WHERE LOGIN = '$l'";
-			$zm = mysqli_query($p, $z2);
-			echo "Zmieniono haslo";
-			header("location: logowanie.php");
-		}
-	else
-		{
-			echo "Stare haslo sie nie zgadza";
-		}
+	if(isset($_POST['h']))
+	{
+		if($row['HASLO'] == $h1) //czy stare haslo zgadza sie tym z bazy danych
+			{
+				if($h1 == $h2)
+				{
+					echo "stare haslo podobne do nowego";
+				}
+				else
+				{
+					$z2 = "UPDATE uzytkownik SET haslo='$h2' WHERE LOGIN = '$l'";
+					$zm = mysqli_query($p, $z2);
+					echo "Zmieniono haslo";
+					header("location: logowanie.php");
+				}
+			}
+		else
+			{
+				echo "Stare haslo sie nie zgadza";
+			}
+	}
 ?>
